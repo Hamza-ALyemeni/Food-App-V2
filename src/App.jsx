52044f62ae13ea4ba88effe2a698b90e4bdbbfa8
Your Name <you@example.com>
2024-10-14 16:08:08 +0300

    using cartcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
+import { CartContextProvider } from "./components/store/CartContext.jsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <Meals />
+      <CartContextProvider>
+        <Header />
+        <Meals />
+      </CartContextProvider>
     </>
   );
 }
